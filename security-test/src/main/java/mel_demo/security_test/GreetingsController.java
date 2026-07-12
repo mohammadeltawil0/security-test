e33ec1bac435fb0f8485c1e4a7e5d29a6669b580
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-07-12 11:50:57 -0400

    Added admin and user, implemented role based authorization

--- security-test/src/main/java/mel_demo/security_test/GreetingsController.java
+++ security-test/src/main/java/mel_demo/security_test/GreetingsController.java
@@ -1,6 +1,7 @@
 package mel_demo.security_test;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,20 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello";
+        return "Hello!";
     }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello, Admin!";
+    }
+
 
 }
